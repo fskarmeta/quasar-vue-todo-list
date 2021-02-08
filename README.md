9d40e7a3cdde2ed8cf1260ced5aea352fa7ebe87
Fabian Skarmeta <fabian.skarmeta@gmail.com>
2021-02-08 18:34:39 -0300

    files saved in repo

---
+++ README.md
@@ -0,0 +1,3 @@
+Todo List made with Quasar and Vue
+It's by itself optimized for Web, IOS, Andriod and Electron.
+I must say, Quasar is very handy :D.
---
+++ src/pages/Help.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>This is the help page</p>
+    <p>I hope you like this todo list :D</p>
+  </q-page>
+</template>
+
+<script>
+export default {};
+</script>
